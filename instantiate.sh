3536e18a1816a3a64ff4ebd182b6217b42bdfaa2
Souf <soufboulealf@gmail.com>
2020-08-27 20:27:41 +0200

    fixes issue with repo url replacement in the pom.xml

--- instantiate.sh
+++ instantiate.sh
@@ -31,8 +31,8 @@ SCRIPT_NAME=$(basename "$0")
 # This is the template name, to be replaced in file names and file
 # content.
 OLD_ROOT=$(basename "$PWD")
+TEMPLATE_FLAG="template-api" 
 
-echo 
 
 if [ "$#" -lt 1 ]; then
     echo "Usage: ./script.sh [api-name]  [api-repo-url]"
@@ -52,38 +52,54 @@ if [ "$NEW_ROOT" == "$OLD_ROOT" ]; then
 fi
 
 # Create root directory
-echo "create new folder ../$OLD_ROOT -> ../$NEW_ROOT"
+echo "* create new folder ../$OLD_ROOT -> ../$NEW_ROOT"
 cp -r "../$OLD_ROOT" "../$NEW_ROOT"
 cd "../$NEW_ROOT"
 
 # Rename files by replacing the project name.
 while IFS= read -r -d '' old_name; do
-    new_name="${old_name//$OLD_ROOT/$NEW_ROOT}"
-    echo "renaming $old_name -> $new_name"
+    new_name="${old_name//$TEMPLATE_FLAG/$NEW_ROOT}"
+    echo "* renaming $old_name -> $new_name"
     mv "$old_name" "$new_name"
-done < <(find ./src -not -path "*/.git*" -type f -name "*${OLD_ROOT}*" -print0)
+done < <(find ./src -not -path "*/.git*" -type f -name "*${TEMPLATE_FLAG}*" -print0)
 
 # Replace project name in src files
 find ./src -type f -print0 | LC_CTYPE=C xargs -0 sed -i '' s/"$OLD_ROOT"/"$NEW_ROOT"/g
+find ./src -type f -print0 | LC_CTYPE=C xargs -0 sed -i '' s/"$TEMPLATE_FLAG"/"$NEW_ROOT"/g
 
 # Replace project name in POM
 find pom.xml -type f -print0 | LC_CTYPE=C xargs -0 sed -i '' s/"$OLD_ROOT"/"$NEW_ROOT"/g
+find pom.xml -type f -print0 | LC_CTYPE=C xargs -0 sed -i '' s/"$TEMPLATE_FLAG"/"$NEW_ROOT"/g
 
-echo "Initializing git"
+echo "* Initializing git..."
 
 git init
 
 if [ -z "$REPO_URL" ]; then
+    echo ""
     echo " ______________________________________________________________________________________________________"
     echo "| !! Attention you didn't provide any repository !!"
     echo "| * Please reconfigure you git remote address using: git remote set-url origin <repo-url>"
     echo "| * Make sure to update the repo url in the pom.xml inside <developerConnection></developerConnection> "
     echo "|______________________________________________________________________________________________________"
 else
-    echo "   Updating pom.xml "
-    find pom.xml -type f -print0 | LC_CTYPE=C xargs -0 sed -i '' s/'[REPLACE_WITH_REPO_URL]'/"$REPO_URL"/g
-    echo "   Updating git remote url"
+    # Escaping the repo url
+    REPO_ESC_URL=""
+    for (( i=0; i<${#REPO_URL}; i++ )); do
+        if [ "${REPO_URL:$i:1}" == '/' ]; then 
+            REPO_ESC_URL="$REPO_ESC_URL\\${REPO_URL:$i:1}"
+        else
+            REPO_ESC_URL="$REPO_ESC_URL${REPO_URL:$i:1}"
+        fi
+    done
+    echo "** Updating pom.xml "
+    sed -i '' "s/\[REPLACE_WITH_REPO_URL\]/$REPO_ESC_URL/g" pom.xml
+    echo "** Updating git remote url"
     git remote set-url origin "$REPO_URL"
+    echo "** Committing initale changes"
+    git commit -am "Project initialization from template"
 fi
 
+echo "* Cleaning..."
+rm -f "$SCRIPT_NAME"
 cd "../$OLD_ROOT"
